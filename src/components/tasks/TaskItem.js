a9bed3ab7526c781b6dff28db912b282ec2b50e9
Constantine <ceeveeya@gmail.com>
2020-08-22 16:39:54 +0300

    Copy task description to clipboard

--- src/components/tasks/TaskItem.js
+++ src/components/tasks/TaskItem.js
@@ -1,17 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
-import { ListGroup, Button } from "react-bootstrap";
+import { ListGroup, Button, Popover, Overlay } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
 import TaskEdit from "./TaskEdit";
 import TaskDelete from "./TaskDelete";
 
 export default function TaskItem({ task, editable }) {
   const [editModalShown, setEditModalShown] = useState(false);
   const [deleteModalShown, setDeleteModalShown] = useState(false);
+  const [showClipboardTooltip, setShowClipboardTooltip] = useState(false);
 
   const showEditModal = () => setEditModalShown(true);
   const showDeleteModal = () => setDeleteModalShown(true);
+
+  const hideClipboardTooltip = () => setShowClipboardTooltip(false);
+  const unhideClipboardTooltip = () => setShowClipboardTooltip(true);
+
+  const descriptionRef = useRef(null);
+  const tooltipTarget = useRef(null);
+
+  const copyToClipboard = () => {
+    let range = document.createRange();
+    range.selectNode(descriptionRef.current);
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // select text
+    document.execCommand("copy");
+
+    window.getSelection().removeAllRanges(); // deselect
+    unhideClipboardTooltip();
+
+    setTimeout(() => {
+      hideClipboardTooltip();
+    }, 1300);
+  };
+
+  const taskDescription = (
+    <span ref={tooltipTarget}>
+      &mdash;{" "}
+      <span ref={descriptionRef} className="pr-2">
+        {task.description}
+      </span>{" "}
+      <FontAwesomeIcon
+        icon={faCopy}
+        style={{ cursor: "pointer" }}
+        title="Copy description to clipboard"
+        onClick={copyToClipboard}
+      />
+    </span>
+  );
+
+  const clipboardTooltip = (
+    <Overlay
+      target={tooltipTarget.current}
+      show={showClipboardTooltip}
+      placement="top"
+      transition={true}
+    >
+      <Popover>
+        <Popover.Content>Task description copied to clipboard!</Popover.Content>
+      </Popover>
+    </Overlay>
+  );
 
   return (
     <ListGroup.Item
@@ -26,7 +77,7 @@ export default function TaskItem({ task, editable }) {
           {task.hours > 0 && task.hours + "h"}{" "}
           {task.minutes > 0 && task.minutes + "m"}
         </b>
-        {task.description && " \u2013 " + task.description}
+        {task.description && taskDescription}
       </div>
       {editable && (
         <div className="justify-content-between" style={{ display: "flex" }}>
@@ -57,6 +108,7 @@ export default function TaskItem({ task, editable }) {
           />
         </div>
       )}
+      {showClipboardTooltip && clipboardTooltip}
     </ListGroup.Item>
   );
 }
